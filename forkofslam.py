4159795c35b09a09c4f8077e5e2a015e05c0d0e3
Nikhil Kumar <nikhilkumar516@gmail.com>
2013-12-19 16:29:40 -0500

    Figured out path...cannot figure out how to do 'post curl -X POST -d 1' in python...might switch to shell

---
+++ forkofslam.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+ 
+import subprocess
+import time
+import urllib2
+import xml.etree.ElementTree as ET
+import os
+import datetime
+ 
+#Location of Stream to be SAVED
+DefaultStation="wtfx-fm"
+ 
+def getXML():
+    data=urllib2.urlopen('http://p2.'+station+'.ccomrcdn.com/player/player_dispatcher.html?section=radio&action=listen_live').read()
+    xml=ET.fromstring(data)
+    return xml
+ 
+def getSongInfo():
+    xml=getXML()
+    artist=xml.find('ListenLiveInitialize/JustPlayed/song/artist').attrib['name']
+    title=xml.find('ListenLiveInitialize/JustPlayed/song/track').attrib['track_title']
+    return artist,title
+ 
+ 
+ 
+while True:
+    station=raw_input("Enter Station ID [" + DefaultStation + "]: ")
+ 
+    if not station:
+        station=DefaultStation
+    try:
+        xml=getXML()
+    except urllib2.URLError:
+        print "Error - Invalid Station ID or Web Server Problem - Try Again"
+    else:
+        break
+ 
+while True:
+    try:
+        TIME=int(raw_input("Time Stream will Play (in minutes): "))
+    except ValueError:
+        print "Error - Invalid Time - Try Again"
+    else:
+        break
+ 
+rtmpurl=xml.find("ListenLiveInitialize/StreamInfo/stream").attrib['primary_location']
+ 
+mp=subprocess.Popen(['/usr/bin/mplayer','http:\\kdge-fm.akacast.akamaistream.net\7\259\20088\v1\auth.akacast.akamaistream.net\kdge-fm', '-novideo', '-ao', 'alsa', '-quiet'], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+mpPID=mp.pid
+ 
+endTime = datetime.datetime.now() + datetime.timedelta(minutes=TIME)
+ 
+OldSongInfo=[]
+ 
+while datetime.datetime.now() < endTime:
+    SongInfo=getSongInfo()
+    if not SongInfo == OldSongInfo:
+        OldSongInfo = SongInfo
+        print SongInfo[0] + " - " + SongInfo[1]
+    time.sleep(5)
+ 
+ 
+ 
+print("Stopping MPlayer...")
+os.kill(mpPID, 2)
+ 
+print "Done"
--- slam.py
+++ slam.py
@@ -1,67 +1,15 @@
-#!/usr/bin/env python
- 
+
+ #streams live music from iheart radio using the rtmp link
+#mplayer "rtmp://cp19971.live.edgefcs.net/live/Nyo_NY_WAXQ-FM_OR@s7907?auth=daEbqdWcRa3c_b2bzaTcjbFdmcKaHdsasdy-bsS09n-4q-RO5X3_6poEEor1HCwkwuyr&aifp=1234&CHANNELID=1465&CPROG=SIMULCAST&MARKET=NEWYORK-NY&REQUESTOR=WAXQ-FM&SERVER_NAME=service.ccrd.clearchannel.com&SITE_ID=1674&STATION_ID=WAXQ-FM&MNM=2&TYPEOFPLAY=0}"
+
 import subprocess
 import time
 import urllib2
-import xml.etree.ElementTree as ET
-import os
-import datetime
- 
-#Location of Stream to be SAVED
-DefaultStation="wtfx-fm"
- 
-def getXML():
-    data=urllib2.urlopen('http://p2.'+station+'.ccomrcdn.com/player/player_dispatcher.html?section=radio&action=listen_live').read()
-    xml=ET.fromstring(data)
-    return xml
- 
-def getSongInfo():
-    xml=getXML()
-    artist=xml.find('ListenLiveInitialize/JustPlayed/song/artist').attrib['name']
-    title=xml.find('ListenLiveInitialize/JustPlayed/song/track').attrib['track_title']
-    return artist,title
- 
- 
- 
-while True:
-    station=raw_input("Enter Station ID [" + DefaultStation + "]: ")
- 
-    if not station:
-        station=DefaultStation
-    try:
-        xml=getXML()
-    except urllib2.URLError:
-        print "Error - Invalid Station ID or Web Server Problem - Try Again"
-    else:
-        break
- 
-while True:
-    try:
-        TIME=int(raw_input("Time Stream will Play (in minutes): "))
-    except ValueError:
-        print "Error - Invalid Time - Try Again"
-    else:
-        break
- 
-rtmpurl=xml.find("ListenLiveInitialize/StreamInfo/stream").attrib['primary_location']
- 
-mp=subprocess.Popen(['/usr/bin/mplayer', rtmpurl, '-novideo', '-ao', 'alsa', '-quiet'], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-mpPID=mp.pid
- 
-endTime = datetime.datetime.now() + datetime.timedelta(minutes=TIME)
- 
-OldSongInfo=[]
- 
-while datetime.datetime.now() < endTime:
-    SongInfo=getSongInfo()
-    if not SongInfo == OldSongInfo:
-        OldSongInfo = SongInfo
-        print SongInfo[0] + " - " + SongInfo[1]
-    time.sleep(5)
- 
- 
- 
-print("Stopping MPlayer...")
-os.kill(mpPID, 2)
- 
-print "Done"
+import json
+
+
+station=raw_input("Enter the 4 digit stream number")
+data = urllib2.urlopen('http://www.iheart.com/a/live/station/'+station+'/stream/','1')
+
+
+
