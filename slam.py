71b77fa44daab9788abd2e5a52a0d35d8170d7ef
Nikhil Kumar <nikhilkumar516@gmail.com>
2013-12-23 16:11:52 -0500

    code works, mplayer not connecting

--- slam.py
+++ slam.py
@@ -4,12 +4,20 @@
 
 import subprocess
 import time
-import urllib2
 import json
-
+import requests
+import pprint
 
 station=raw_input("Enter the 4 digit stream number")
-data = urllib2.urlopen('http://www.iheart.com/a/live/station/'+station+'/stream/','1')
+data = requests.post('http://www.iheart.com/a/live/station/'+station+'/stream/',stream=True).json()
+rtmp = data[u'stream_urls'][u'rtmp']
+name = data[u'info'][u'station_name']
+#cmd = 'rtmpdump -r '+rtmp + ' -v | mplayer' 
+#subprocess.call(cmd.split(),shell=False)
+mp=subprocess.Popen(['mplayer', rtmp, '-novideo', '-ao', 'alsa'], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+mpPID=mp.pid
+
+
 
 
 
